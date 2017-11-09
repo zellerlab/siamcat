8f9ce6fe86ecb44a5d36faf8b1c24af87dcd0528
Konrad Zych <dominesum@gmail.com>
2017-11-09 21:07:17 +0100

    new file for CI

---
+++ Rscript_flavor/00_setup.r
@@ -0,0 +1,27 @@
+###
+# SIAMCAT -  Statistical Inference of Associations between Microbial Communities And host phenoTypes
+# RScript flavor
+#
+# written by Georg Zeller
+# with additions by Nicolai Karcher and Konrad Zych
+# EMBL Heidelberg 2012-2017
+#
+# version 0.2.0
+# file last updated: 09.11.2017
+# GNU GPL 3.0
+###
+
+package.list <- c("RColorBrewer",
+                  "beanplot",
+                  "glmnet",
+                  "LiblineaR",
+                  "pROC",
+                  "optparse",
+                  "colorRamps",
+                  "gelnet",
+                  "mlr")
+
+notInst      <- which(!package.list%in%installed.packages())
+if(length(notInst)>0) install.packages(package.list[notInst])
+
+if(!"SIAMCAT"%in%installed.packages()) install.packages("SIAMCAT_0.2.0.tar.gz",repos=NULL, type="source")
