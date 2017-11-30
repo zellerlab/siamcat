edf7a136e1bad2758600f8388b3c619bbd2fa793
Konrad Zych <dominesum@gmail.com>
2017-11-30 15:06:15 +0100

    better R script formatting

--- Rscript_flavor/11_evaluate_predictions.r
+++ Rscript_flavor/11_evaluate_predictions.r
@@ -23,53 +23,45 @@ suppressMessages(library('SIAMCAT'))
   make_option('--pred',               type='character',                help='Input file containing the trained classification model(s)'),
   make_option('--plot',               type='character',                help='Output file for plotting'),
   make_option('--write_eval_results', type='logical',   default=FALSE, help='Should calculated parameters be written into tab-delimited file? (Necessary for generation of test files'),
-  make_option('--output_results',     type='character', default="eval_results.tsv",  help='Output file containing evaluation results (only necessary when write_eval_results is set to TRUE'),
-  make_option('--model_type',         type='character',                help='Model type')
+  make_option('--output_results',     type='character', default="eval_results.tsv",  help='Output file containing evaluation results (only necessary when write_eval_results is set to TRUE')
   )
 
 # parse arguments
 opt            <- parse_args(OptionParser(option_list=option_list))
-source.dir     <- opt$srcdir
-fn.label       <- opt$label
-fn.pred        <- opt$pred
-fn.plot        <- opt$plot
-write.out      <- opt$write_eval_results
-output.results <- opt$output_results
-model.type     <- opt$model_type
-
 cat("=== 11_model_evaler.r\n")
 cat("=== Paramaters of the run:\n\n")
-cat('source.dir     =', source.dir, '\n')
-cat('fn.label       =', fn.label, '\n')
-cat('fn.pred        =', fn.pred, '\n')
-cat('fn.plot        =', fn.plot, '\n')
-cat('write.out      =', write.out, '\n')
-cat('output.results =', output.results, '\n')
+cat('srcdir             =', opt$srcdir, '\n')
+cat('label              =', opt$label, '\n')
+cat('pred               =', opt$pred, '\n')
+cat('plot               =', opt$plot, '\n')
+cat('write_eval_results =', opt$write_eval_results, '\n')
+cat('output_results     =', opt$output_results, '\n')
 cat('\n')
 
 ### If variable source.dir does not end with "/", append "/" to end of source.dir
-source.dir <- appendDirName(source.dir)
+source.dir <- appendDirName(opt$srcdir)
 start.time <- proc.time()[1]
-label      <- read.labels(fn.label)
+label      <- read.labels(opt$label)
 
 
-pred <- read.table(file=fn.pred, sep='\t', header=FALSE, row.names=1, check.names=FALSE, quote='')
-if (ncol(pred) > 1) {
-  pred <- read.table(file=fn.pred, sep='\t', header=TRUE, row.names=1, check.names=FALSE, quote='')
-}
+pred <- read.table(file=opt$pred, sep='\t', header=FALSE, row.names=1, check.names=FALSE, quote='')
 pred <- as.matrix(pred)
 
-eval.data <-  eval.result(label, pred)
+eval.data <-  eval.result(label=label,
+                          pred=pred)
 
-evaluation.model.plot(label, pred, eval.data, model.type, fn.plot=fn.plot)
+evaluation.model.plot(label=label,
+                      pred=pred, 
+                      eval.data=eval.data,
+                      fn.plot=opt$plot)
 
 
 # The following code writes calculated auroc and aupr- values into a file for testing.
-if (write.out == TRUE){
+if (opt$write_eval_results == TRUE){
   # Testing only makes sense if dim(pred)[2] > 1
   if(ncol(pred) == 1){
-    write.table(t(aucs), file=output.results, quote=FALSE, sep='\t', col.names=FALSE, append=FALSE, row.names="auroc values")
-    write.table(t(aucspr), file=output.results, quote=FALSE, sep='\t', col.names=FALSE, append=TRUE, row.names="auprc values")
+    write.table(t(aucs), file=opt$output_results, quote=FALSE, sep='\t', col.names=FALSE, append=FALSE, row.names="auroc values")
+    write.table(t(aucspr), file=opt$output_results, quote=FALSE, sep='\t', col.names=FALSE, append=TRUE, row.names="auprc values")
   }else{
     cat("Only one prediction available, ignoring the write_eval_results option.\n")
   }
