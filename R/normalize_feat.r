27525a908ea266027434d82b33aaa1f0232f11ef
Konrad Zych <dominesum@gmail.com>
2017-12-01 11:38:30 +0100

    bugfix

--- R/normalize_feat.r
+++ R/normalize_feat.r
@@ -35,8 +35,8 @@
 #'  \item \code{$par} <- parameters utilized in the normalization;
 #'  \item \code{$feat} <- normalized features
 #'}
-normalize.feat <- function(feat, norm.method <- c("rank.unit", "rank.std", "log.std", "log.unit"), log.n0 <- 10^-8, sd.min.q <- 0.1, n.p <- 2,
-                           n.sample <- FALSE, n.feature <- TRUE, n.global <- FALSE) {
+normalize.feat <- function(feat, norm.method = c("rank.unit", "rank.std", "log.std", "log.unit"), log.n0 = 10^-8, sd.min.q = 0.1, n.p = 2,
+                           n.sample = FALSE, n.feature = TRUE, n.global = FALSE) {
   ### remove features with missing values
   # TODO there may be better ways of dealing with NA features
   # TODO 2 add defaults for the parameters!!! Not all parameters are needed for all normalization methods
