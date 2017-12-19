17bf6aeb86ae5ccee745ca17272968c619e8094d
KerstenBreuer <kersten-breuer@outlook.com>
2017-12-19 15:54:40 +0100

    corrected command line argument

--- Rscript_flavor/00_setup.r
+++ Rscript_flavor/00_setup.r
@@ -23,17 +23,12 @@ package.list <- c("RColorBrewer",
                   "mlr",
                   "PRROC")
 
-# The location of the siamcat package can be specified as command line argument after '--package_path'.
-# By default the package is assumed to be located in './SIAMCAT_0.2.0.tar.gz'.
+# script can take the location of the siamcat package as argument
+# by default it is assumed to be located in './SIAMCAT_0.2.0.tar.gz'
 args = commandArgs(trailingOnly = FALSE)
-if(length(args)==0) {
-    package.path <- "./SIAMCAT_0.2.0.tar.gz"
-    
-} else {
-    package.path <- if(args=="--package_path") args[ which(args=="--package_path")+1 ] else "./SIAMCAT_0.2.0.tar.gz"
-}
+package.path <- if(length(args)==0) "./SIAMCAT_0.2.0.tar.gz" else args[1]
 
 notInst      <- which(!package.list%in%installed.packages())
 if(length(notInst)>0) install.packages(package.list[notInst], repos="http://cran.uni-muenster.de")
 
-if(!"SIAMCAT"%in%installed.packages()) install.packages(pkgs=package.path, repos=NULL, type="source")
+if(!"SIAMCAT"%in%installed.packages()) install.packages(package.path, repos=NULL, type="source")
