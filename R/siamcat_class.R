7b73ccb6d9229ab4a28e12de41c0011b67db32ed
Konrad Zych <dominesum@gmail.com>
2018-01-17 11:15:32 +0100

    building siamcat class

--- R/siamcat_class.R
+++ R/siamcat_class.R
@@ -6,7 +6,20 @@
 # GNU GPL 3.0
 ###
 
+#' The S4 class for storing taxa-abundance information and models.
 #' @name siamcat-class
 #' @rdname siamcat-class
 #' @exportClass siamcat
 setClass("siamcat", representation(modelList = "list", phyloseq = "phyloseq"))
+
+#' Build siamcat-class objects from their components.
+#' @name siamcat
+#' @export
+
+siamcat <- function(...){
+  arglist   <- list(...)
+  modelList <- list(NULL)
+  
+  sc        <- new("siamcat", name = "Hadley", phyloseq = ps)
+  return(sc)
+}
