ee505e05adb9b8fd1d5a53b3c09b0693f884785b
Konrad Zych <dominesum@gmail.com>
2018-02-16 16:20:15 +0100

    verbose

--- R/check_confounders.r
+++ R/check_confounders.r
@@ -13,12 +13,15 @@
 #' @keywords SIAMCAT confounder.check
 #' @export
 #' @return Does not return anything, but produces a single plot for each metadata category
-confounder.check <- function(siamcat, fn.plot){
+confounder.check <- function(siamcat, fn.plot, verbose=1){
+  if(verbose>1) cat("+ starting confounder.check\n")
+  s.time <- proc.time()[3]
   # TODO: implement color.scheme selection as function parameter
   pdf(fn.plot, onefile = TRUE)
   case.count <- length(siamcat@label@label[siamcat@label@p.idx])
   ctrl.count <- length(siamcat@label@label[siamcat@label@n.idx])
   
+  if(verbose>2) cat("+++ checking group sizes\n")
   if (case.count > ctrl.count) {
     lgr <- siamcat@label@p.idx
     smlr <- siamcat@label@n.idx
@@ -29,6 +32,7 @@ confounder.check <- function(siamcat, fn.plot){
     bp.labs <- c(siamcat@label@n.lab, siamcat@label@p.lab)
   }
   
+  if(verbose>2) cat("+++ setting up color scheme\n")
   len.diff <- abs(case.count - ctrl.count)
   hmap <- data.frame()
   hmapcolors <- colorRampPalette(brewer.pal(10, "RdYlGn"))
@@ -37,7 +41,7 @@ confounder.check <- function(siamcat, fn.plot){
   for (m in 1:ncol(siamcat@phyloseq@sam_data)) {
     mname <- gsub('[_.-]', ' ', colnames(siamcat@phyloseq@sam_data)[m])
     mname <- paste(toupper(substring(mname, 1, 1)), substring(mname, 2), sep="")
-    cat('checking', mname, 'as a potential confounder...\n')
+    if(verbose>1) cat('+++ checking', mname, 'as a potential confounder\n')
     
     mvar  <- as.numeric(unlist(siamcat@phyloseq@sam_data[,m]))
     u.val <- unique(mvar)
@@ -45,9 +49,9 @@ confounder.check <- function(siamcat, fn.plot){
     colors <- brewer.pal(5, "Spectral")
     histcolors <- brewer.pal(9, "YlGnBu")
     if (length(u.val) == 1) {
-      cat('  skipped because all subjects have the same value\n')
+      if(verbose>1) cat('+++  skipped because all subjects have the same value\n')
     } else if (length(u.val) <= 5) {
-      cat('  using a bar plot\n')
+      if(verbose>1) cat('++++ discreet variable, using a bar plot\n')
       
       ct <- matrix(NA, nrow = 2, ncol = length(u.val))
       nms <- c()
@@ -64,6 +68,7 @@ confounder.check <- function(siamcat, fn.plot){
         freq[,i] <- freq[,i] / sum(freq[,i])
       }
       
+      if(verbose>2) cat('++++ plotting barplot\n')
       # packages required for grid + base
       layout(matrix(c(1,1,2)))
       # barplot
@@ -77,6 +82,7 @@ confounder.check <- function(siamcat, fn.plot){
       mtext(paste('Fisher test p-value:', format(p.val, digits=4)), cex=0.6, side=1, line=2)
       popViewport()
       
+      if(verbose>2) cat('++++ drawing contingency table\n')
       # contingency table
       plot.new()
       vps <- baseViewports()
@@ -90,7 +96,7 @@ confounder.check <- function(siamcat, fn.plot){
       popViewport()
       par(mfrow=c(1,1), bty="o")
     } else {
-      cat('  using a Q-Q plot\n')
+      if(verbose>1) cat('++++ continuous variable, using a Q-Q plot\n')
       # discretize continuous variable; split at median for now
       dct <- matrix(NA, nrow = 2, ncol = 2)
       dct[1,] <- c(sum(mvar[siamcat@label@n.idx] <= median(mvar, na.rm=TRUE), na.rm=TRUE), sum(mvar[siamcat@label@p.idx] <= median(mvar, na.rm=TRUE), na.rm=TRUE))
@@ -99,7 +105,7 @@ confounder.check <- function(siamcat, fn.plot){
       hmap <- rbind(hmap, dct)
       layout(rbind(c(1,2), c(3,4)))
       
-      # Q-Q plot
+      if(verbose>2) cat('++++ panel 1/4: Q-Q plot\n')
       par(mar=c(4.5, 4.5, 2.5, 1.5),mgp=c(2.5,1,0))
       ax.int <- c(min(mvar, na.rm=TRUE), max(mvar, na.rm=TRUE))
       qqplot(mvar[siamcat@label@n.idx], mvar[siamcat@label@p.idx], xlim=ax.int, ylim=ax.int, pch=16, cex=0.6,
@@ -109,13 +115,13 @@ confounder.check <- function(siamcat, fn.plot){
       text(ax.int[1]+0.9*(ax.int[2]-ax.int[1]), ax.int[1]+0.1*(ax.int[2]-ax.int[1]), cex = 0.8,
            paste('MWW test p-value:', format(p.val, digits=4)), pos=2)
       
-      # X histogram
+      if(verbose>2) cat('++++ panel 2/4: X histogram\n')
       par(mar=c(4, 2.5, 3.5, 1.5))
       hist(mvar[siamcat@label@n.idx], main = siamcat@label@n.lab, xlab = mname, col = histcolors, 
            breaks = seq(min(mvar, na.rm=TRUE), max(mvar, na.rm=TRUE), length.out=10))
       mtext(paste('N =', length(mvar[siamcat@label@n.idx])),cex=0.6,side=3,adj=1,line=1)
       
-      # X boxplot
+      if(verbose>2) cat('++++ panel 3/4: X boxplot\n')
       par(mar=c(2.5, 4.5, 2.5, 1.5))
       #combine <- list(mvar[siamcat@label@n.idx], mvar[siamcat@label@p.idx])
       combine <- data.frame(mvar[lgr], c(mvar[smlr], rep(NA, len.diff)))
@@ -126,7 +132,7 @@ confounder.check <- function(siamcat, fn.plot){
       #stripchart(combine, vertical=TRUE, add=TRUE, method="jitter", pch=20)
       stripchart(combine, vertical=TRUE, add=TRUE, method="jitter", pch=20)
       
-      # Y histogram
+      if(verbose>2) cat('++++ panel 4/4: Y histogram\n')
       par(mar=c(4.5, 2.5, 3.5, 1.5))
       hist(mvar[siamcat@label@p.idx], main = siamcat@label@p.lab, xlab = mname, col = histcolors, 
            breaks = seq(min(mvar, na.rm=TRUE), max(mvar, na.rm=TRUE), length.out=10))
@@ -136,24 +142,28 @@ confounder.check <- function(siamcat, fn.plot){
   }
   
   ## HEATMAP / SPECTRAL PLOT... TBD
-  
-  # transform heatmap values
-  colnames(hmap) <- c(siamcat@label@n.lab, siamcat@label@p.lab)
-  hmap.ratios <- as.matrix(log(hmap[,2]/hmap[,1])) - log(sum(siamcat@label@p.idx)/sum(siamcat@label@p.idx))
-  rownames(hmap.ratios) <- as.matrix(rownames(hmap))
-  
-  # plot as labeled barplot...
-  par(mar=c(20, 5, 10, 5))
-  grad.len <- 1000
-  barplot(as.matrix(rep(1,grad.len)), col = viridis(grad.len), horiz=TRUE, border=0, ylab='', axes=FALSE)
-  bp.bound <- max(abs(range(hmap.ratios[is.finite(hmap.ratios)])))
-  bp.ticks <- seq(-round(bp.bound), round(bp.bound), length.out=7)
-  bp.label <- paste('Relative ratios for metadata,', siamcat@label@p.lab,':',siamcat@label@n.lab)
-  axis(1, at=seq(0,grad.len,length.out=7), labels=bp.ticks, cex.axis=0.7)
-  
-  # draw lines to represent each metadata category
-  for (val in hmap.ratios) {
-    abline(v=(val*grad.len/6) + (grad.len/2))
+  if(0){
+    # transform heatmap values
+    colnames(hmap) <- c(siamcat@label@n.lab, siamcat@label@p.lab)
+    hmap.ratios <- as.matrix(log(hmap[,2]/hmap[,1])) - log(sum(siamcat@label@p.idx)/sum(siamcat@label@p.idx))
+    rownames(hmap.ratios) <- as.matrix(rownames(hmap))
+    
+    # plot as labeled barplot...
+    par(mar=c(20, 5, 10, 5))
+    grad.len <- 1000
+    barplot(as.matrix(rep(1,grad.len)), col = viridis(grad.len), horiz=TRUE, border=0, ylab='', axes=FALSE)
+    bp.bound <- max(abs(range(hmap.ratios[is.finite(hmap.ratios)])))
+    bp.ticks <- seq(-round(bp.bound), round(bp.bound), length.out=7)
+    bp.label <- paste('Relative ratios for metadata,', siamcat@label@p.lab,':',siamcat@label@n.lab)
+    axis(1, at=seq(0,grad.len,length.out=7), labels=bp.ticks, cex.axis=0.7)
+    
+    # draw lines to represent each metadata category
+    for (val in hmap.ratios) {
+      abline(v=(val*grad.len/6) + (grad.len/2))
+  }
   }
   tmp <- dev.off()
+  e.time <- proc.time()[3]
+  if(verbose>1) cat("+ finished confounder.check in",e.time-s.time,"s\n")
+  if(verbose==1)cat("Checking metadata for confounders finished\n")
 }
