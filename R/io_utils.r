9fee95aa189bc63ce4aebcdf60da50ce2ddbd9f8
Jakob Wirbel <jakob.wirbel@embl.de>
2018-03-26 10:44:29 +0200

    updated docu a bit, removed deprecated function parse.model.header.

--- R/io_utils.r
+++ R/io_utils.r
@@ -12,10 +12,13 @@
 #' The file should be oragnized as follows:
 #' features (in rows) x samples (in columns).
 #'
-#' First row should contain sample labels (consistent with label data), while the first column should contain feature labels (e.g. taxonomic identifiers). The remaining entries are expected to be real values \code{>= 0} that quantify the abundance of each feature in each sample.
+#' First row should contain sample labels (consistent with label data), while
+#' the first column should contain feature labels (e.g. taxonomic identifiers).
+#' The remaining entries are expected to be real values \code{>= 0} that quantify
+#' the abundance of each feature in each sample.
 #' @param fn.in.feat name of the tsv file containing features
 #' @export
-#' @return matrix containing features from the file
+#' @return \code{otu_table} containing features from the file
 read.features <- function(fn.in.feat, verbose=0){
   if(verbose>1) cat("+ starting read.features\n")
   s.time <- proc.time()[3]
@@ -36,16 +39,21 @@ read.features <- function(fn.in.feat, verbose=0){
 }
 
 #' @title Read labels file
-#' @description This file reads in the tsv file with labels and converts it into a matrix.
+#' @description This file reads in the tsv file with labels and converts it into
+#'  a label object.
 #'
-#' First row is expected to be \code{#BINARY:1=[label for cases];-1=[label for controls]}. Second row should contain the sample identifiers as tab-separated list (consistent with feature and metadata).
+#' First row is expected to be \code{#BINARY:1=[label for cases];-1=[label for controls]}.
+#' Second row should contain the sample identifiers as tab-separated list
+#' (consistent with feature and metadata).
 #'
-#' Third row is expected to contain the actual class labels (tab-separated): \code{1} for each case and \code{-1} for each control.
+#' Third row is expected to contain the actual class labels (tab-separated):
+#' \code{1} for each case and \code{-1} for each control.
 #'
-#' Note: Labels can take other numeric values (but not characters or strings); importantly, the label for cases has to be greater than the one for controls.
+#' Note: Labels can take other numeric values (but not characters or strings);
+#' importantly, the label for cases has to be greater than the one for controls.
 #' @param fn.in.label name of the tsv file containing labels
 #' @export
-#' @return list with nine values:\itemize{
+#' @return label object containing several entries:\itemize{
 #' \item \code{$label} named vector containing the numerical labels from the file;
 #' \item \code{$header} first row of the label file;
 #' \item \code{$info} information about the type of label (e.g. \code{BINARY});
@@ -56,8 +64,7 @@ read.features <- function(fn.in.feat, verbose=0){
 #' \item \code{$p.idx} logical vector of labels (\code{TRUE} for cases, \code{FALSE} otherwise);
 #' \item \code{$p.lab} label for cases, e.g. \code{cancer}
 #'}
-read.labels <- function(fn.in.label,feat=NULL){
-  # TODO move feature/label agreement check to validate data?
+read.labels <- function(fn.in.label){
   if (is.null(fn.in.label)) stop("Filename for labels file not provided!\n")
   if(!file.exists(fn.in.label)) stop("Label file ", fn.in.label, " does not exist!\n")
   label <- read.table(file=fn.in.label, sep='\t', header=TRUE, row.names=NULL, stringsAsFactors=FALSE,
@@ -74,9 +81,6 @@ read.labels <- function(fn.in.label,feat=NULL){
   namesL          <- colnames(label)
   label           <- as.numeric(label)
   names(label)    <- namesL
-  if(!all(is.null(feat))){
-    if(any(names(label) != colnames(feat))) stop("read.labels: Names in label object and feat object do not match!\n")
-  }
 
   # Check general suitablity of supplied dataset
   classes <- unique(label)
@@ -105,8 +109,8 @@ read.labels <- function(fn.in.label,feat=NULL){
 
   label$p.idx <- label$label==label$positive.lab
   label$p.lab <- gsub('[_.-]', ' ', names(label$info$class.descr)[label$info$class.descr==label$positive.lab])
-  
-  labelRes <- new("label", label = label$label, header = label$header, info=label$info, 
+
+  labelRes <- new("label", label = label$label, header = label$header, info=label$info,
                   positive.lab=label$positive.lab,
                   negative.lab=label$negative.lab, n.idx=label$n.idx, p.idx=label$p.idx,
                   n.lab=label$n.lab, p.lab=label$p.lab)
@@ -114,18 +118,21 @@ read.labels <- function(fn.in.label,feat=NULL){
 }
 
 #' @title Read metadata file
-#' @description This file reads in the tsv file with numerical metadata and converts it into a matrix.
+#' @description This file reads in the tsv file with numerical metadata and
+#' converts it into a matrix.
 #'
 #' The file should be organized as follows:
-#' samples (in rows) x metadata (in columns). Metadata needs to be converted to numerical values by the user.
+#' samples (in rows) x metadata (in columns). Metadata needs to be converted to
+#' numerical values by the user.
 #'
-#' Metadata may be optional for the SIAMCAT workflow, but are necessary for heatmap displays, see \link{interpretor.model.plot}
+#' Metadata may be optional for the SIAMCAT workflow, but are necessary for
+#' heatmap displays, see \link{model.interpretation.plot}
 #' @param fn.in.meta name of the tsv file containing metadata
 #' @export
-#' @return matrix containing metadata from the file
+#' @return \code{sample_data} object
 read.meta <- function(fn.in.meta){
   if (is.null(fn.in.meta) || toupper(fn.in.meta)=='NULL' || toupper(fn.in.meta)=='NONE' || toupper(fn.in.meta)=='UNKNOWN') {
-    cat("Filename for metadata file not provided, continuing without it.\n")
+    warning("Filename for metadata file not provided, continuing without it.\n")
   }else{
     if(!file.exists(fn.in.meta)) stop("Metadata file ", fn.in.meta, " does not exist!\n")
     meta <- read.table(file=fn.in.meta, sep='\t', header=TRUE, row.names=1, check.names=FALSE, quote='')
@@ -161,32 +168,4 @@ parse.label.header <- function(label.header) {
   label.info$type <- type
   label.info$class.descr <- class.descr
   return(label.info)
-}
-
-# ##### function to parse the header of a model file
-#' @keywords internal
-parse.model.header <- function(model.header) {
-  s <- strsplit(model.header, ':')[[1]]
-  type <- trim(s[1])
-  if (substr(type, 1, 1) == '#')
-    type <- trim(substr(type, 2, nchar(type)))
-  label.header <- trim(paste(s[2:length(s)], collapse=':'))
-  if (substr(label.header, 1, 1) == '[') {
-    stopifnot(substr(label.header, nchar(label.header), nchar(label.header)) == ']')
-    label.header <- substr(label.header, 2, nchar(label.header)-1)
-  }
-  p <- grep('\\(.*\\)', type)
-  properties <- NULL
-  if (length(p) > 0) {
-    stopifnot(length(p) == 1)
-    stopifnot(substr(type, nchar(type), nchar(type)) == ')')
-    properties <- substr(type, p+1, nchar(type)-1)
-    type <- trim(substr(type, 1, p-1))
-  }
-
-  model.info <- list()
-  model.info$type <- type
-  model.info$properties <- properties
-  model.info$label.header <- label.header
-  return(model.info)
 }
