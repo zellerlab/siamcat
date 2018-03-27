9c8437a72be6ee41cc9e5b740de4b125a6e48a31
Jakob Wirbel <jakob.wirbel@embl.de>
2018-03-27 19:03:18 +0200

    added docu for verbose paramter.

--- R/siamcat_class_methods.R
+++ R/siamcat_class_methods.R
@@ -10,6 +10,8 @@
 #' @description This functions filters \code{siamcat@label}.
 #' @param siamcat an object of class \link{siamcat}
 #' @param ids names of samples to be left in the \code{siamcat@label}
+#' @param verbose control output: \code{0} for no output at all, \code{1} for more
+#'  information about progress and success, defaults to \code{1}
 #' @keywords filter.label
 #' @export
 #' @return siamcat an object of class \link{siamcat}
@@ -56,35 +58,35 @@ setMethod("show", "siamcat", function(object){
   if(length(object@eval_data)){
     cat(paste("eval_data()             eval_data:         Average AUC:",round(object@eval_data$auc.average[[1]],3), sep = " "), fill = TRUE)
   }
-  
+
   # print otu_table (always there).
   cat("\ncontains phyloseq-class experiment-level object @phyloseq:", fill=TRUE)
-  cat(paste("phyloseq@otu_table()   OTU Table:         [ ", ntaxa(otu_table(object@phyloseq)), " taxa and ", 
-            nsamples(otu_table(object@phyloseq)), " samples ]", sep = ""), fill = TRUE)	
-  
+  cat(paste("phyloseq@otu_table()   OTU Table:         [ ", ntaxa(otu_table(object@phyloseq)), " taxa and ",
+            nsamples(otu_table(object@phyloseq)), " samples ]", sep = ""), fill = TRUE)
+
   # print Sample Data if there
   if(!is.null(sample_data(object@phyloseq, FALSE))){
-    cat(paste("phyloseq@sam_data()    Sample Data:       [ ", dim(sample_data(object@phyloseq))[1], " samples by ", 
-              dim(sample_data(object@phyloseq))[2], 
+    cat(paste("phyloseq@sam_data()    Sample Data:       [ ", dim(sample_data(object@phyloseq))[1], " samples by ",
+              dim(sample_data(object@phyloseq))[2],
               " sample variables ]", sep = ""), fill = TRUE)
   }
-  
-  # print tax Tab if there	
+
+  # print tax Tab if there
   if(!is.null(tax_table(object@phyloseq, FALSE))){
-    cat(paste("phyloseq@tax_table()   Taxonomy Table:    [ ", dim(tax_table(object@phyloseq))[1], " taxa by ", 
-              dim(tax_table(object@phyloseq))[2], 
+    cat(paste("phyloseq@tax_table()   Taxonomy Table:    [ ", dim(tax_table(object@phyloseq))[1], " taxa by ",
+              dim(tax_table(object@phyloseq))[2],
               " taxonomic ranks ]", sep = ""), fill = TRUE)
   }
-  
+
   # print tree if there
   if(!is.null(phy_tree(object@phyloseq, FALSE))){
-    cat(paste("phyloseq@phy_tree()    Phylogenetic Tree: [ ", ntaxa(phy_tree(object@phyloseq)), " tips and ", 
+    cat(paste("phyloseq@phy_tree()    Phylogenetic Tree: [ ", ntaxa(phy_tree(object@phyloseq)), " tips and ",
               phy_tree(object@phyloseq)$Nnode,
               " internal nodes ]", sep = ""),
         fill = TRUE
-    ) 
+    )
   }
-  
+
   # print refseq summary if there
   if(!is.null(refseq(object@phyloseq, FALSE))){
     cat(paste("phyloseq@refseq()      ", class(refseq(object@phyloseq))[1], ":      [ ", ntaxa(refseq(object@phyloseq)), " reference sequences ]", sep = ""), fill=TRUE)
