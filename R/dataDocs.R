54837afa87f99b7074d26167758ffeab523f8ee1
Jakob Wirbel <jakob.wirbel@embl.de>
2018-03-28 13:19:13 +0200

    extended docu for dataset.

--- R/dataDocs.R
+++ R/dataDocs.R
@@ -1,13 +1,11 @@
 #' Documentation for the example siamcat object in the data folder
 #'
+#' Reduced version of the CRC dataset in inst/extdata, containing 100 features
+#'  and 80 samples, saved after the complete SIAMCAT pipeline has been run.
+#'  Therefore, contains entries in every siamcat-object slot, e.g, \code{eval_data}
+#'  or \code{data_split}. Mainly used for running the examples in the function
+#'  documentation
 #' @name siamcat_example
-#' @docType data
-#' @keywords data
-NULL
-
-#' Documentation for the example siamcat object in the data folder
-#'
-#' @name siamcat_simple
 #' @docType data
 #' @keywords data
 NULL
