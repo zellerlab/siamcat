fe11f4a86231b85bd55862ee49f5718f950a00fe
Konrad Zych <dominesum@gmail.com>
2018-04-06 22:15:06 +0200

    roxygenise

--- R/siamcat_class_constructor.R
+++ R/siamcat_class_constructor.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 ### SIAMCAT - Statistical Inference of Associations between Microbial
-### Communities And host phenoTypes R flavor EMBL
-### Heidelberg 2012-2018 GNU GPL 3.0
+### Communities And host phenoTypes
+### EMBL Heidelberg 2012-2018 GNU GPL 3.0
 
 #' Build siamcat-class objects from their components.
 #' @title siamcat
