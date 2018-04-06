9316d22136d4914b2cd928d3b9b23b28aaff8843
Konrad Zych <dominesum@gmail.com>
2018-04-06 21:01:33 +0200

    assignment functions

--- R/assign_methods.R
+++ R/assign_methods.R
@@ -23,3 +23,26 @@ setGeneric("phyloseq<-", function(x, value) standardGeneric("phyloseq<-"))
 setMethod("phyloseq<-", c("siamcat","phyloseq"), function(x, value){
   siamcat(value, x@model_list, x@eval_data, x@label, x@norm_param, x@data_split, x@pred_matrix)
 })
+
+
+################################################################################
+#' Assign a new label object to \code{x}
+#'
+#' @usage label(x) <- value
+#'
+#' @param x an object of class \link{siamcat-class}
+#' @param value an object of class \link{label-class}
+#' @export
+#' @docType methods
+#' @rdname assign-label
+#' @aliases assign-label
+#'
+#' @examples
+#' # data(siamcat_example)
+setGeneric("label<-", function(x, value) standardGeneric("label<-"))
+#' @rdname assign-label
+#' @aliases label<-
+setMethod("label<-", c("siamcat","label"), function(x, value){
+  siamcat(value, x@model_list, x@eval_data, x@phyloseq, x@orig_feat, 
+          x@norm_param, x@data_split, x@pred_matrix)
+})
