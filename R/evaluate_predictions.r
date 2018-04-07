851c5ae2ae366dd3a9c2f326de92829287566c01
Konrad Zych <dominesum@gmail.com>
2018-04-07 23:19:16 +0200

    attempting bugfix

--- R/evaluate_predictions.r
+++ R/evaluate_predictions.r
@@ -58,12 +58,13 @@
 evaluate.predictions <- function(siamcat, verbose = 1) {
     if (verbose > 1) 
         cat("+ starting evaluate.predictions\n")
+    label      <- get.label.list(siamcat)
     s.time <- proc.time()[3]
     # TODO compare header to label make sure that label and prediction are in the same order
-    m <- match(names(siamcat@label@label), rownames(pred_matrix(siamcat)))
+    m <- match(names(label$label), rownames(pred_matrix(siamcat)))
     
     pred <- pred_matrix(siamcat)[m, , drop = FALSE]
-    stopifnot(all(names(siamcat@label@label) == rownames(pred)))
+    stopifnot(all(names(label$label) == rownames(pred)))
     
     # ROC curve
     if (verbose > 2) 
@@ -73,18 +74,18 @@ evaluate.predictions <- function(siamcat, verbose = 1) {
         rocc = list(NULL)
         aucs = vector("numeric", ncol(pred))
         for (c in 1:ncol(pred)) {
-            rocc[c] = list(roc(response = siamcat@label@label, predictor = pred[, c], ci = FALSE))
+            rocc[c] = list(roc(response = label$label, predictor = pred[, c], ci = FALSE))
             aucs[c] = rocc[[c]]$auc
         }
-        l.vec = rep(siamcat@label@label, ncol(pred))
+        l.vec = rep(label$label, ncol(pred))
     } else {
-        l.vec = siamcat@label@label
+        l.vec = label$label
     }
     # average data for plotting one mean prediction curve
     if (verbose > 2) 
         cat("+ calculating mean ROC\n")
     summ.stat = "mean"
-    rocsumm = list(roc(response = siamcat@label@label, predictor = apply(pred, 1, summ.stat), ci = TRUE, of = "se", 
+    rocsumm = list(roc(response = label$label, predictor = apply(pred, 1, summ.stat), ci = TRUE, of = "se", 
         sp = seq(0, 1, 0.05)))
     auroc = list(rocsumm[[1]]$auc)
     # precision recall curve
@@ -93,13 +94,13 @@ evaluate.predictions <- function(siamcat, verbose = 1) {
     if (ncol(pred) > 1) {
         aucspr = vector("numeric", dim(pred)[2])
         for (c in 1:ncol(pred)) {
-            ev[c] = list(evaluate.classifier(pred[, c], siamcat@label@label, siamcat@label, verbose = verbose))
+            ev[c] = list(evaluate.classifier(pred[, c], label$label, siamcat@label, verbose = verbose))
             pr[c] = list(evaluate.get.pr(ev[[c]], verbose = verbose))
             aucspr[c] = evaluate.calc.aupr(ev[[c]], verbose = verbose)
         }
-        ev = append(ev, list(evaluate.classifier(apply(pred, 1, summ.stat), siamcat@label@label, siamcat@label)))
+        ev = append(ev, list(evaluate.classifier(apply(pred, 1, summ.stat), label$label, siamcat@label)))
     } else {
-        ev[1] = list(evaluate.classifier(as.vector(pred), siamcat@label@label, siamcat@label, verbose = verbose))
+        ev[1] = list(evaluate.classifier(as.vector(pred), label$label, siamcat@label, verbose = verbose))
         pr[1] = list(evaluate.get.pr(ev[[1]]), verbose = verbose)
     }
     if (ncol(pred) > 1) {
