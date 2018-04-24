07c024f01864ffbf144ab47ba98aba18a7f8f385
Konrad Zych <dominesum@gmail.com>
2018-04-24 09:52:12 +0200

    function to save siamcat object to lefgse format

--- R/io_utils.r
+++ R/io_utils.r
@@ -336,3 +336,43 @@ create.label.from.metadata <- function(meta, column) {
         )
     return(labelRes)
 }
+
+#' @title create a lefse input file from siamcat object
+#' 
+#' @description This function creates a lefse input file from siamcat object
+#' 
+#' @param siamcat object of class \link{siamcat-class}
+#' 
+#' @param filename name of the input file to which data will be save
+#' 
+#' @keywords internal
+#' 
+#' @return nothing but data is written to a file
+#' 
+#' @export
+#' 
+siamcat.to.lefse <- function(siamcat, filename) {
+    feat   <- get.features.matrix(siamcat)
+    label  <- label(siamcat)
+    meta   <- t(meta(siamcat))
+    labelD <- label$label
+    labelD[label$p.idx] <- label$p.lab
+    labelD[label$n.idx] <- label$n.lab
+
+    results <- rbind(labelD,
+                        meta,
+                        colnames(feat),
+                        feat)
+
+    rownames(results) <- c("label",
+                            rownames(meta),
+                            "sample_id",
+                            rownames(feat))
+
+    write.table(results,
+                file = filename,   
+                quote = FALSE, 
+                sep = '\t', 
+                row.names = TRUE,
+                col.names = FALSE)
+}
