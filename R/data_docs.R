d6eefb21b64d81c49fb8e02f3078764efa4d4aec
Jakob Wirbel <jakob.wirbel@embl.de>
2019-03-22 10:50:47 +0100

    updated documentation for the included data.

--- R/data_docs.R
+++ R/data_docs.R
@@ -1,38 +1,50 @@
-#' Documentation for the example siamcat object in the data folder
+#' @title SIAMCAT example
 #'
-#' Reduced version of the CRC dataset in inst/extdata, containing 100 features
-#' (15 associated features at 5\% FDR in the original dataset and 85 random
-#' other features) and 141 samples, saved after the complete SIAMCAT
+#' @description Reduced version of the CRC dataset from Zeller et al. MSB 2014
+#' (see \url{http://msb.embopress.org/content/10/11/766}), containing 100
+#' features (15 associated features at 5\% FDR in the original dataset and
+#' 85 random other features) and 141 samples, saved after the complete SIAMCAT
 #' pipelinehas been run.
-#' Therefore, contains entries in every siamcat-object slot,
-#' e.g, \code{eval_data} or \code{data_split}. Mainly used for running
-#' the examples in the function documentation
+#'
+#' Thus, the example dataset contains entries in every slot of the SIAMCAT
+#' object (see \link{siamcat-class}), e.g, \code{eval_data} or
+#' \code{data_split}.
+#'
+#' Mainly used for running the examples in the function documentation.
 #'
 #' @name siamcat_example
 #'
+#' @source \url{http://msb.embopress.org/content/10/11/766}
+#'
 #' @docType data
 #'
 #' @keywords data
 NULL
 
-#' Documentation for the example feature object in the data folder
+#' @title Example feature matrix
 #'
-#' Feature matrix (as data.frame) for the CRC dataset, containing 141 samples
-#' and 1754 bacterial species (features).
+#' @description Feature matrix (as data.frame) of the CRC dataset from Zeller
+#' et al. MSB 2014 (see \url{http://msb.embopress.org/content/10/11/766}),
+#' containing 141 samples and 1754 bacterial species (features).
 #'
 #' @name feat.crc.zeller
 #'
+#' @source \url{http://msb.embopress.org/content/10/11/766}
+#'
 #' @docType data
 #'
 #' @keywords data
 NULL
 
-#' Documentation for the example metadata object in the data folder
+#' @title Example metadata matrix
 #'
-#' Metadata (as data.frame) for the CRC dataset, containing 6 variables (e.g.
-#' Age or BMI) for 141 samples.
+#' @description Metadata (as data.frame) of the CRC dataset from Zeller et
+#' al. MSB 2014 (see \url{http://msb.embopress.org/content/10/11/766}),
+#' containing 6 metadata variables variables (e.g. Age or BMI) for 141 samples.
 #'
 #' @name meta.crc.zeller
+#'
+#' @source \url{http://msb.embopress.org/content/10/11/766}
 #'
 #' @docType data
 #'
