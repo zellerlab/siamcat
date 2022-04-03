d08ee5f97d0605808adae155d85fafdac18e253a
Jakob Wirbel <jakob.wirbel@embl.de>
2022-04-03 18:13:38 +0200

    import mlr3 and other packages.

--- R/import_packages.r
+++ R/import_packages.r
@@ -1,18 +1,17 @@
-#' @import     phyloseq mlr LiblineaR methods
-#' @rawNamespace import(glmnet, except = 'auc')
+#' @import     phyloseq mlr3 mlr3learners mlr3tuning methods paradox 
+#'           glmnet LiblineaR
 #' @importFrom grDevices col2rgb colorRampPalette dev.off gray pdf rgb
 #' @importFrom graphics abline arrows axis barplot box strheight strwidth
 #'           boxplot grid image layout lines mtext hist points stripchart
 #'           par plot plot.new polygon rect text title segments legend symbols
 #' @importFrom stats coefficients cor median p.adjust rnorm qqplot
 #'           predict quantile runif sd wilcox.test addmargins na.omit
-#'           fisher.test binomial coef confint glm profile
+#'           fisher.test binomial coef confint glm profile lm as.formula
 #' @importFrom utils head read.table tail
 #' @importFrom PRROC pr.curve
 #' @importFrom pROC roc
 #' @importFrom beanplot beanplot
 #' @importFrom RColorBrewer brewer.pal brewer.pal.info
-#' @importFrom ParamHelpers makeParamSet makeDiscreteParam makeNumericParam
 #' @importFrom grid popViewport pushViewport plotViewport
 #' @importFrom gridBase baseViewports
 #' @importFrom gridExtra ttheme_minimal grid.table
@@ -21,8 +20,10 @@
 #' @importFrom scales alpha
 #' @importFrom utils write.table read.csv askYesNo
 #' @importFrom stringr str_extract_all str_split
-#' @importFrom infotheo condentropy discretize
+#' @importFrom infotheo condentropy discretize mutinformation
 #' @importFrom corrplot corrplot colorlegend
 #' @importFrom progress progress_bar
+#' @importFrom lmerTest lmer
+#' @importFrom lgr get_logger
 
 NULL
